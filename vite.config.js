2c51f09d681683ef5cd581b69481594d4fc8b064
rares1215 <126170459+rares1215@users.noreply.github.com>
2023-11-01 14:16:56 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/myCv/",
   plugins: [react()],
-})
+  base: "/myCv/", // Set the base URL for your website
+  build: {
+    outDir: "dist", // Output directory for the built files
+    assetsInlineLimit: 0,
+  },
+});
